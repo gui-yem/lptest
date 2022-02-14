1ff56f60ab5588cf7ea7867da24ba3b4d646e967
gui-yem <guilhem.chevallier@gmail.com>
2022-02-14 21:42:37 +0100

    création des joueurs (fixtures)

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,6 +2,7 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Player;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
@@ -9,8 +10,13 @@ class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        // create 20 players
+        for ($i = 0; $i < 20; $i++) {
+            $player = new Player();
+            $player->setName('joueur  ' . $i);
+            $player->setPower(mt_rand(10, 100));
+            $manager->persist($player);
+        }
 
         $manager->flush();
     }
